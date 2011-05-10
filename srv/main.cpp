f6d4baf9baab24f6b0df7f0930ac7b6b4d31f377
Kai-Feng <maple@maple-asus.(none)>
2011-05-10 22:51:39 +0800

    first initialize version commit

---
+++ srv/main.cpp
@@ -0,0 +1,118 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <concurrency/ThreadManager.h>
+#include <concurrency/PosixThreadFactory.h>
+#include <protocol/TBinaryProtocol.h>
+#include <server/TSimpleServer.h>
+#include <server/TThreadPoolServer.h>
+#include <server/TThreadedServer.h>
+#include <transport/TServerSocket.h>
+#include <transport/TTransportUtils.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+
+#include "gen-cpp/PostSystem.h"
+
+using namespace std;
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+using namespace apache::thrift::server;
+
+using namespace boost;
+
+using namespace tw::maple::generated;
+
+class PostSystemHandler : virtual public PostSystemIf {
+ public:
+  PostSystemHandler() {
+    // Your initialization goes here
+  }
+
+  void newPost(UUID& _return, const std::string& user, const std::string& content) {
+    // Your implementation goes here
+    printf("newPost\n");
+  }
+
+  bool deletePost(const UUID& post_id) {
+    // Your implementation goes here
+    printf("deletePost\n");
+  }
+
+  void newComment(UUID& _return, const UUID& post_id, const std::string& comment) {
+    // Your implementation goes here
+    printf("newComment\n");
+  }
+
+  bool rmComment(const UUID& post_id, const UUID& comment) {
+    // Your implementation goes here
+    printf("rmComment\n");
+  }
+
+  void getContent(std::string& _return, const UUID& post_id) {
+    // Your implementation goes here
+    printf("getContent\n");
+  }
+
+};
+
+int main(int argc, char **argv) {
+
+  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+  shared_ptr<PostSystemHandler> handler(new PostSystemHandler());
+  shared_ptr<TProcessor> processor(new PostSystemProcessor(handler));
+  shared_ptr<TServerTransport> serverTransport(new TServerSocket(9090));
+  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+
+  TSimpleServer server(processor,
+                       serverTransport,
+                       transportFactory,
+                       protocolFactory);
+
+
+  /**
+   * Or you could do one of these
+
+  shared_ptr<ThreadManager> threadManager =
+    ThreadManager::newSimpleThreadManager(workerCount);
+  shared_ptr<PosixThreadFactory> threadFactory =
+    shared_ptr<PosixThreadFactory>(new PosixThreadFactory());
+  threadManager->threadFactory(threadFactory);
+  threadManager->start();
+  TThreadPoolServer server(processor,
+                           serverTransport,
+                           transportFactory,
+                           protocolFactory,
+                           threadManager);
+
+  TThreadedServer server(processor,
+                         serverTransport,
+                         transportFactory,
+                         protocolFactory);
+
+  */
+
+  printf("Starting the server...\n");
+  server.serve();
+  printf("done.\n");
+  return 0;
+}
---
+++ thrift/proto.thrift
@@ -0,0 +1,17 @@
+
+namespace java tw.maple.generated
+namespace cpp tw.maple.generated
+
+typedef string UUID
+
+service PostSystem
+{
+    UUID    newPost( 1: string user, 2: string content ),
+    bool    deletePost( 1: UUID post_id ),
+
+    UUID    newComment( 1: UUID post_id, 2: string comment ),
+    bool    rmComment( 1: UUID post_id, 2: UUID comment ),
+
+    string  getContent( 1: UUID post_id ),
+}
+
